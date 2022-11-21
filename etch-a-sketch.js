574dcab58e39c6c6badc411cdfdb2395565e3dfb
gameandwebtester <elaskowski@hotmail.ca>
2022-11-21 04:52:05 -0500

    basic funtionality

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,15 +1,46 @@
 
 const container = document.querySelector('#container');
 
-const num = 16;
+let slider = document.getElementById('slider')
+let output = document.getElementById("demo");
+output.innerHTML = slider.value;
 
+let num = slider.value;
+
+slider.onchange = function() {
+    output.innerHTML = this.value;
+}
+
+console.log(num);
+
+container.style.setProperty("--n", num);
 
 function createDivs(){
-    for(let i = 0; i < num; i++) {
+    for(let i = 0; i < num * num; i++) {
         const innerDivs = document.createElement('div');
-        innerDivs.textContent = 'this is a test';
+        innerDivs.className = 'squares';
+        innerDivs.textContent = '';
         container.appendChild(innerDivs)[i];
     }
 }
 
 createDivs();
+
+let test = document.getElementsByClassName("squares");
+
+function changeSquares() {
+    for (let i = 0; i < test.length; i++) {
+        test[i].addEventListener('click', function onClick() {
+            test[i].style.backgroundColor = 'black';
+        })
+    }
+}
+
+changeSquares();
+
+function clearButton(){
+    for (let i = 0; i < test.length; i++) {
+            test[i].style.backgroundColor = 'white';
+    }
+}
+

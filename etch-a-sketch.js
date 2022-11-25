0130ff9c2e536b5670287ff8747731bec279c3f4
gameandwebtester <elaskowski@hotmail.ca>
2022-11-24 20:56:47 -0500

    got rainbow mode working and black bottons working

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -6,16 +6,20 @@ let output = document.getElementById("demo");
 let test = document.getElementsByClassName("squares");
 output.innerHTML = slider.value;
 
+let isEnabled = false;
+
 let num = slider.value;
 
 slider.onchange = function() {
     output.innerHTML = this.value;
     num = this.value;
     container.style.setProperty("--n", num);
+    //always remove previous squares before generating new ones
     removeSquares();
     createDivs();
-    changeSquares();
 }
+
+container.style.setProperty("--n", num);
 
 function createDivs(){
     for(let i = 0; i < num * num; i++) {
@@ -23,25 +27,49 @@ function createDivs(){
         innerDivs.className = 'squares';
         innerDivs.textContent = '';
         container.appendChild(innerDivs)[i];
-        console.log(num);
     }
 }
+
+createDivs();
+
 
 function removeSquares(){
     document.querySelectorAll('.squares').forEach(e => e.remove());
 }
 
 function changeSquares() {
-    for (let i = 0; i < test.length; i++) {
-        test[i].addEventListener('click', function onClick() {
-            test[i].style.backgroundColor = 'black';
-        })
+    if(isEnabled == true){
+        for (let i = 0; i < test.length; i++) {
+            test[i].addEventListener('mouseover', function onClick() {
+                test[i].style.backgroundColor = 'black'
+            });
+        }
     }
 }
 
+function randomColor(){
+
+    const roundValue = Math.round, rndmValue = Math.random, maxNum = 255;
+
+    for (let i = 0; i < test.length; i++) {
+        test[i].addEventListener('mouseover', function onClick() {
+            test[i].style.backgroundColor = 'rgba(' + roundValue(rndmValue()*maxNum) + ',' + roundValue(rndmValue()*maxNum) + ',' + roundValue(rndmValue()*maxNum) + ')';
+        });
+    }
+}
+
+function enableThings(){
+    isEnabled = true;
+    changeSquares();
+}
+
+
 function clearButton(){
+    isEnabled = false;
     for (let i = 0; i < test.length; i++) {
             test[i].style.backgroundColor = 'white';
     }
 }
+
+
 

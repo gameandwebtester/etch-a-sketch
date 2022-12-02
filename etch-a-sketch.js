cef91d7b39d64a25d6a06ef78432445a3a3bea65
gameandwebtester <elaskowski@hotmail.ca>
2022-12-01 21:19:28 -0500

    fixed some bugs with black and color squares

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -13,15 +13,16 @@ let squares = document.getElementsByClassName("squares");
 
 let isActive = false;
 
-output.innerHTML = slider.value;
+output.innerHTML = slider.value + ' x ' + slider.value;
 
 let num = slider.value;
 
 
 slider.onchange = function() {
-    output.innerHTML = this.value;
+    output.innerHTML = this.value + ' x ' + this.value;
     num = this.value;
     container.style.setProperty("--n", num);
+
     //always remove previous squares before generating new ones
     removeSquares();
     createDivs();
@@ -47,10 +48,18 @@ function removeSquares(){
 
 //document.getElementById("blackmode").onclick = function() {enable()};
 
-function activate(){
+function activateBlack(){
     isActive = true;
+    //boxes.forEach(equip);
+    console.log('black mode');
+    blackSquares();
+}
 
-    //blackSquares();
+function activateRandom(){
+    isActive = true;
+    //boxes.forEach(equip);
+    console.log('black mode');
+    randomColor();
 }
 
 console.log(isActive);
@@ -60,9 +69,11 @@ function blackSquares() {
     if(isActive === true){
         console.log(isActive + ' sdsd');
         for (let i = 0; i < squares.length; i++) {  
-            squares[i].addEventListener('mousedown', makeBlack);
+            squares[i].addEventListener('mousedown', event => {
+                if(event.buttons === 1 && isActive === true) makeBlack(event);
+            });
             squares[i].addEventListener('mouseover' , event => {
-                if(event.buttons == 1)makeBlack(event);
+                if(event.buttons == 1 && isActive === true)makeBlack(event);
         } );
     }
 }
@@ -75,32 +86,31 @@ const boxes = [...document.getElementsByClassName("squares")];
 const makeBlack2 = event => event.target.style.backgroundColor = "black";
 
 function equip(box) {
-    if(isActive){
-        
-    }
-        box.addEventListener("mousedown", makeBlack2);
+
+        box.addEventListener("mousedown", event => {
+            if(event.buttons === 1 && isActive === true) makeBlack(event);
+        });
         box.addEventListener("mouseover", event => {
             if (event.buttons === 1 && isActive === true) makeBlack2(event);
   });
 }
-boxes.forEach(equip);
 
 //................................................end
 
-
- 
-
 function randomColor(){
     for (let i = 0; i < squares.length; i++) {
-        squares[i].addEventListener('mousedown', makeRandomColor);
+        squares[i].addEventListener('mousedown', event => {
+            if(event.buttons === 1 && isActive === true) makeRandomColor(event);
+        });
         squares[i].addEventListener('mouseover', event => {
-            if(event.buttons == 1) makeRandomColor(event);
+            if(event.buttons == 1 && isActive === true) makeRandomColor(event);
         })
     }
 }
 
 function clearButton(){
     isActive = false;
+    console.log(isActive);
     for (let i = 0; i < squares.length; i++) {
         squares[i].style.backgroundColor = 'white';
     }
